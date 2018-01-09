197a3d8c1c0ec77a1d3dd1eca3eaeb474a429e33
TeonChien <teon0327@hotmail.com>
2018-01-09 14:21:02 +0800

    V1.2 2018/1/9
    按鈕按了可到其他Activity，依照地區不同會有中文英文差別

--- AndroidProject/feature/src/main/java/com/example/teon/myapplication/feature/MainActivity.java
+++ AndroidProject/feature/src/main/java/com/example/teon/myapplication/feature/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.teon.myapplication.feature;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void general(View view) {
+        Intent intent = new Intent(this, GeneralActivity.class);
+        startActivity(intent);
+    }
+
+    public void advanced(View view) {
+        Intent intent = new Intent(this, AdvancedActivity.class);
+        startActivity(intent);
+    }
+
+    public void recommend(View view) {
+        Intent intent = new Intent(this, RecommendActivity.class);
+        startActivity(intent);
+    }
+
+    public void gps(View view) {
+        Intent intent = new Intent(this, GPSActivity.class);
+        startActivity(intent);
     }
 }
