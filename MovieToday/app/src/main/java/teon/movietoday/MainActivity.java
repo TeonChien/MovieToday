97fa0958b529a3e08e2b2f8b907c3346c74823d0
TeonChien <teon0327@hotmail.com>
2018-01-15 12:01:36 +0800

    2018/01/15 MovieToday V1.0
    更換成API19

---
+++ MovieToday/app/src/main/java/teon/movietoday/MainActivity.java
@@ -0,0 +1,35 @@
+package teon.movietoday;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void general(View view) {
+        Intent intent = new Intent(this, GeneralActivity.class);
+        startActivity(intent);
+    }
+
+    public void advanced(View view) {
+        Intent intent = new Intent(this, AdvancedActivity.class);
+        startActivity(intent);
+    }
+
+    public void recommend(View view) {
+        Intent intent = new Intent(this, RecommendActivity.class);
+        startActivity(intent);
+    }
+
+    public void gps(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+}
